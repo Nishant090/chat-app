f65662bbe309ce645d2fa84a5a590236fbd69d44
Nishant <nishantkhatiwada6@gmail.com>
2023-07-09 17:20:24 +0545

    for hosting

---
+++ public/db.js
@@ -0,0 +1,10 @@
+var FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB-WKSipVkx-Gk3FZmsJoCmLR6TriL0C7A",
+  authDomain: "video-chat-f98f7.firebaseapp.com",
+  databaseURL: "https://video-chat-f98f7-default-rtdb.firebaseio.com",
+  projectId: "video-chat-f98f7",
+  storageBucket: "video-chat-f98f7.appspot.com",
+  messagingSenderId: "278475959041",
+  appId: "1:278475959041:web:f00539ed0b6ff1b3bcc900",
+  measurementId: "G-KXDWGW4R16"
+   };
